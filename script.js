f864ccdb7c6b1a81b5d29e867d72e10e9f2b4023
Mykyta Vasyljev <mykyta.vasyljev@gmail.com>
2018-10-04 20:00:43 +0300

    v 1.0

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.scroll_to').click(function(e){
+        var jump = $(this).attr('href');
+        var new_position = $(jump).offset();
+       $('html, body').stop().animate({ scrollTop: new_position.top }, 800);
+       e.preventDefault();
+    });
+
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('#arrow-to-top').fadeIn();
+		} else {
+			$('#arrow-to-top').fadeOut();
+		}
+	});
+
+	$('#arrow-to-top-link').click(function() {
+		$('body, html').animate({scrollTop:0},800);
+	});
+
+});
